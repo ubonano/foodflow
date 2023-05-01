3dee945103e6e19911dd0e40c856148a82628f40
Uriel Bonano <ubonano@gmail.com>
2023-04-30 21:10:11 -0300

    refactor service order repository

--- lib/order/service_order_repository.dart
+++ lib/order/service_order_repository.dart
@@ -1,22 +1,38 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:logging/logging.dart';
 import 'service_order_model.dart';
 
 class ServiceOrderRepository {
-  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  final FirebaseFirestore _firestore;
+  final Logger _logger;
+  final String _collectionName = 'serviceOrders';
+
+  ServiceOrderRepository(
+      {required FirebaseFirestore firestore, required Logger logger})
+      : _firestore = firestore,
+        _logger = logger;
 
   Stream<List<ServiceOrder>> getOrdersStream() {
-    return _firestore.collection('serviceOrders').snapshots().map((snapshot) {
-      return snapshot.docs.map((doc) {
-        return ServiceOrder.fromDocumentSnapshot(doc);
-      }).toList();
-    });
+    _logger.info('Fetching orders stream from Firestore.');
+    return _firestore
+        .collection(_collectionName)
+        .snapshots()
+        .map(_snapshotToServiceOrderList);
+  }
+
+  List<ServiceOrder> _snapshotToServiceOrderList(QuerySnapshot snapshot) {
+    _logger.info('Converting snapshot to order list.');
+    return snapshot.docs
+        .map((doc) => ServiceOrder.fromDocumentSnapshot(doc))
+        .toList();
   }
 
   Future<void> createOrder(ServiceOrder order) async {
+    _logger.info('Creating new order: ${order.toMap()}');
     try {
-      await _firestore.collection('serviceOrders').add(order.toMap());
+      await _firestore.collection(_collectionName).add(order.toMap());
     } catch (e) {
-      print('Error al crear la orden: $e');
+      _logger.severe('Error creating the order: $e');
       throw e;
     }
   }
--- lib/setup/get_it_setup.dart
+++ lib/setup/get_it_setup.dart
@@ -1,12 +1,20 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:get_it/get_it.dart';
+import 'package:logging/logging.dart';
 import '../order/service_order_controller.dart';
 import '../order/service_order_repository.dart';
 
 GetIt getIt = GetIt.instance;
 
+final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
 void setupGetIt() {
   getIt.registerLazySingleton<ServiceOrderRepository>(
-      () => ServiceOrderRepository());
+    () => ServiceOrderRepository(
+      firestore: _firestore,
+      logger: Logger('ServiceOrderRepository'),
+    ),
+  );
   getIt.registerLazySingleton<ServiceOrderController>(
     () => ServiceOrderController(
       orderRepository: getIt<ServiceOrderRepository>(),

828e0c03e32ca11a499e9764b2ec61dda0d4c6b7
Uriel Bonano <ubonano@gmail.com>
2023-04-30 21:00:04 -0300

    add logs to service order controller

--- lib/order/service_order_controller.dart
+++ lib/order/service_order_controller.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'package:rxdart/rxdart.dart';
+import 'package:logging/logging.dart';
 import 'service_order_model.dart';
 import 'service_order_repository.dart';
 
@@ -8,18 +9,23 @@ enum ServiceOrderSortingOption { time, tableNumber, waiterName }
 class ServiceOrderController {
   final ServiceOrderRepository _serviceOrderRepository;
   final BehaviorSubject<ServiceOrderSortingOption> _sortingOptionsSubject;
+  final Logger _logger = Logger('ServiceOrderController');
 
   ServiceOrderController({required orderRepository})
       : _serviceOrderRepository = orderRepository,
         _sortingOptionsSubject =
             BehaviorSubject<ServiceOrderSortingOption>.seeded(
-                ServiceOrderSortingOption.time);
+                ServiceOrderSortingOption.time) {
+    _logger.info('ServiceOrderController created.');
+  }
 
   void updateSortingOption(ServiceOrderSortingOption sortingOption) {
+    _logger.info('Updating sorting option to: $sortingOption');
     _sortingOptionsSubject.add(sortingOption);
   }
 
   Stream<List<ServiceOrder>> getOrdersStream() {
+    _logger.info('Fetching orders stream.');
     return Rx.combineLatest2<List<ServiceOrder>, ServiceOrderSortingOption,
             List<ServiceOrder>>(_serviceOrderRepository.getOrdersStream(),
         _sortingOptionsSubject.stream, _sortOrders);
@@ -27,6 +33,7 @@ class ServiceOrderController {
 
   List<ServiceOrder> _sortOrders(
       List<ServiceOrder> orders, ServiceOrderSortingOption sortingOption) {
+    _logger.info('Sorting orders by: $sortingOption');
     List<ServiceOrder> sortedOrders = List<ServiceOrder>.from(orders);
     switch (sortingOption) {
       case ServiceOrderSortingOption.tableNumber:
@@ -43,10 +50,12 @@ class ServiceOrderController {
   }
 
   Future<void> createOrder(ServiceOrder order) async {
+    _logger.info('Creating new order: ${order.toMap()}');
     await _serviceOrderRepository.createOrder(order);
   }
 
   void dispose() {
+    _logger.info('Closing ServiceOrderController and releasing resources.');
     _sortingOptionsSubject.close();
   }
 }

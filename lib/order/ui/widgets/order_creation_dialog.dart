ce4e39f670fbe0d1e53bfcc169649639e6e592fb
Uriel Bonano <ubonano@gmail.com>
2023-05-03 15:12:55 -0300

    loading

--- lib/order/ui/widgets/order_creation_dialog.dart
+++ lib/order/ui/widgets/order_creation_dialog.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:foodflow/setup/get_it_setup.dart';
 import '../../../widgets/app_form_fields.dart';
-import '../../order_model.dart';
 import '../../order_controller.dart';
+import '../../order_model.dart';
 
 class OrderCreationDialog extends StatefulWidget {
   const OrderCreationDialog({Key? key}) : super(key: key);
@@ -14,10 +14,13 @@ class OrderCreationDialog extends StatefulWidget {
 class _OrderCreationDialogState extends State<OrderCreationDialog> {
   final _formKey = GlobalKey<FormState>();
   final _orderController = getIt<OrderController>();
+  bool _isCreatingOrder = false;
 
   final _tableNumberController = TextEditingController();
   final _waiterNameController = TextEditingController();
   final _numberOfGuestsController = TextEditingController();
+
+  Future<ServiceOrder?>? _createOrderFuture;
 
   @override
   void dispose() {
@@ -49,39 +52,62 @@ class _OrderCreationDialogState extends State<OrderCreationDialog> {
       ),
       actions: <Widget>[
         TextButton(
+          onPressed: _createOrderFuture == null
+              ? () {
+                  Navigator.of(context).pop();
+                }
+              : null,
           child: const Text('Cancelar'),
-          onPressed: () {
-            Navigator.of(context).pop();
-          },
         ),
-        TextButton(
-          child: const Text('Crear'),
-          onPressed: () async {
-            if (_formKey.currentState!.validate()) {
-              int tableNumber = int.parse(_tableNumberController.text);
-
-              bool isTableTaken =
-                  await _orderController.isTableNumberTaken(tableNumber);
-
-              if (isTableTaken) {
-                ScaffoldMessenger.of(context).showSnackBar(
-                  SnackBar(
-                    content:
-                        Text('La mesa número $tableNumber ya tiene una orden.'),
-                  ),
-                );
-              } else {
-                final newOrder = ServiceOrder.createNew(
-                  tableNumber: tableNumber,
-                  waiterName: _waiterNameController.text,
-                  numberOfGuests: int.parse(_numberOfGuestsController.text),
-                );
-                Navigator.of(context).pop();
-              }
+        FutureBuilder<ServiceOrder?>(
+          future: _createOrderFuture,
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return CircularProgressIndicator();
+            } else {
+              return ElevatedButton(
+                onPressed: () async {
+                  if (_formKey.currentState!.validate()) {
+                    setState(() {
+                      _createOrderFuture = _createOrder();
+                    });
+                  }
+                },
+                child: const Text('Crear'),
+              );
             }
           },
         ),
       ],
     );
+  }
+
+  Future<ServiceOrder?> _createOrder() async {
+    int tableNumber = int.parse(_tableNumberController.text);
+    bool isTableTaken = await _orderController.isTableNumberTaken(tableNumber);
+    if (isTableTaken) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('La mesa número $tableNumber ya tiene una orden.'),
+        ),
+      );
+      return null;
+    } else {
+      final newOrder = ServiceOrder.createNew(
+        tableNumber: tableNumber,
+        waiterName: _waiterNameController.text,
+        numberOfGuests: int.parse(_numberOfGuestsController.text),
+      );
+      await _orderController.createOrder(newOrder);
+      Navigator.of(context).pop();
+
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text(
+              'Se ha creado una nueva orden para la mesa número $tableNumber.'),
+        ),
+      );
+      return newOrder;
+    }
   }
 }

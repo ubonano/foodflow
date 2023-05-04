de9625f434f2f7a45ec171feaad6a9443f50196b
Uriel Bonano <ubonano@gmail.com>
2023-05-03 22:08:08 -0300

    Delete order

--- lib/setup/get_it_setup.dart
+++ lib/setup/get_it_setup.dart
@@ -1,23 +1,21 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:get_it/get_it.dart';
 import 'package:logging/logging.dart';
-import '../order/order_controller.dart';
-import '../order/order_repository.dart';
 
 GetIt getIt = GetIt.instance;
 
 final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
 void setupGetIt() {
-  getIt.registerLazySingleton<OrderRepository>(
-    () => OrderRepository(
-      firestore: _firestore,
-      logger: Logger('ServiceOrderRepository'),
-    ),
-  );
-  getIt.registerLazySingleton<OrderController>(
-    () => OrderController(
-      orderRepository: getIt<OrderRepository>(),
-    ),
-  );
+  // getIt.registerLazySingleton<OrderRepository>(
+  //   () => OrderRepository(
+  //     firestore: _firestore,
+  //     logger: Logger('ServiceOrderRepository'),
+  //   ),
+  // );
+  // getIt.registerLazySingleton<OrderController>(
+  //   () => OrderController(
+  //     orderRepository: getIt<OrderRepository>(),
+  //   ),
+  // );
 }
